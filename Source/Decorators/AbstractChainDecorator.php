b289133b4b7dfb0e863825c6ad8d06c68aaee7a3
Alexey Puchkov <alexey.pkv@gmail.com>
2019-09-12 13:39:00 +0300

    Add chain decorator

---
+++ Source/Decorators/AbstractChainDecorator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Gazelle\Decorators;
+
+
+use Gazelle\AbstractConnectionDecorator;
+use Gazelle\IRequestParams;
+use Gazelle\IResponseData;
+
+
+abstract class AbstractChainDecorator extends AbstractConnectionDecorator
+{
+	protected function before(IRequestParams $requestParams): void {}
+	protected function after(IResponseData $requestParams): void {}
+	protected function onError(IRequestParams $requestParams, \Throwable $t): void {}
+	protected function finally(IRequestParams $requestParams, ?IResponseData $responseData, ?\Throwable $t): void {}
+	
+	
+	public function request(IRequestParams $requestData): IResponseData
+	{
+		$responseData = null;
+		$error = null;
+		
+		$this->before($requestData);
+		
+		try
+		{
+			$responseData = $this->invokeChild($requestData);
+			$this->after($responseData);
+		}
+		catch (\Throwable $error)
+		{
+			$this->onError($requestData, $error);
+		}
+		finally
+		{
+			$this->finally($requestData, $responseData, $error);
+		}
+		
+		return $responseData;
+	}
+}
